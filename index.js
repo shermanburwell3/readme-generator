3d1aa05c19cefd43d3b0ec807e4658aea962632f
Sherman Burwell <shermanburwell3@gmail.com>
2024-07-09 21:22:18 -0500

    Added comments to lay out questions relating to readme generation

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inq = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    //Project title
+
+
+    //Description
+
+    
+    //Steps for Installation
+
+
+    //Usage
+
+
+    //Credits
+
+
+    //License
+
+
+    //Features
+
+
+    //Contribution Guidelines
+
+
+    //Questions and Contact Info
+
+
+    //License
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
